621912fd6c3dc686405d806a9e770b5d80c7080c
wookyoung <pd1125@naver.com>
2021-08-20 01:15:21 +0900

    TEST:secure

--- Algorism/src/practice_backjoon/test.java
+++ Algorism/src/practice_backjoon/test.java
@@ -5,7 +5,6 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("git Test Success?");
-		System.out.println("git Test Success?");
 	}
 
 }
