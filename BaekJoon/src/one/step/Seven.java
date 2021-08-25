25fa04d3440a7b89926a2291ca8997e09d1565ae
wookyoung <pd1125@naver.com>
2021-08-25 23:55:50 +0900

    SETP1:ten

--- BaekJoon/src/one/step/Seven.java
+++ BaekJoon/src/one/step/Seven.java
@@ -2,14 +2,14 @@ package one.step;
 //두 수를 입력받고 합을 출력하는 문제
 import java.util.Scanner;
 
-public class five {
+public class Seven {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int A = scan.nextInt();
 		int B = scan.nextInt();
 		if(A > 0 && B <10) {
-			System.out.println(A+B);
+			System.out.println((double)A/B);
 		}
 	}
 
--- BaekJoon/src/one/step/Ten.java
+++ BaekJoon/src/one/step/Ten.java
@@ -2,14 +2,15 @@ package one.step;
 //두 수를 입력받고 합을 출력하는 문제
 import java.util.Scanner;
 
-public class five {
+public class Ten {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int A = scan.nextInt();
 		int B = scan.nextInt();
-		if(A > 0 && B <10) {
-			System.out.println(A+B);
+		int C = scan.nextInt();
+		if(2 <= A && B && C <=10) {
+			System.out.println((double)A/B);
 		}
 	}
 
