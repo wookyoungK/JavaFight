a222c6919732c145595a2b8aab44c4658ee3c0be
wookyoung <pd1125@naver.com>
2021-08-29 23:54:34 +0900

    STEP1:10번여러가지방법

---
+++ BaekJoon/src/one/ten/TenToOne.java
@@ -0,0 +1,40 @@
+package one.ten;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class TenToOne {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+ 		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str," ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+        
+		System.out.println( (A+B)%C );
+		System.out.println( (A%C + B%C)%C );
+		System.out.println( (A*B)%C );
+		System.out.println( (A%C * B%C)%C );
+	
+/*    
+굳이 String 변수 생성 안하고 입력과 동시에 구분자로 분리해줘도 된다.
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+        
+		System.out.println( (A+B)%C );
+		System.out.println( (A%C + B%C)%C );
+		System.out.println( (A*B)%C );
+		System.out.println( (A%C * B%C)%C );
+*/
+		
+	}
+
+}
