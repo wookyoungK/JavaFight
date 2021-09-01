882c01f1b8ec3d9e7c3519c779c647089f940575
wookyoung <pd1125@naver.com>
2021-09-01 23:50:31 +0900

    STEP2:START

---
+++ BaekJoon/src/two/test.java
@@ -0,0 +1,16 @@
+package two;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class test {
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println((333%100)/10);
+	}
+
+}
