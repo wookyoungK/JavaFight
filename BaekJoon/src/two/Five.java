e0ab9e244870280e34518c9b62b2bf771ac359fb
wookyoung <pd1125@naver.com>
2021-09-02 23:55:23 +0900

    STEP2:finish

---
+++ BaekJoon/src/two/Five.java
@@ -0,0 +1,19 @@
+package two;
+
+import java.util.Scanner;
+
+public class Five {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+		
+		if(M<45 && H !=0) {
+			System.out.println(H-1+" "+((M+60)-45));
+		}else {
+			System.out.println(H+" "+(M-65));
+		}
+	}
+
+}
