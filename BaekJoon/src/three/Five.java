c53dd4de18c143115f793b8f8404b10b9694fdc7
wookyoung <pd1125@naver.com>
2021-09-09 23:59:02 +0900

    STEP3:ing

--- BaekJoon/src/three/Five.java
+++ BaekJoon/src/three/Five.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Four {
+public class Five {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,11 +15,9 @@ public class Four {
 
 		int A = Integer.parseInt(br.readLine());
 //		String str = br.readLine();
-		StringTokenizer st;
 
 		for (int i = 0; i < A; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n");
+			bw.write(i+1+"\n");
 		}
 		br.close();
 		
--- BaekJoon/src/three/Seven.java
+++ BaekJoon/src/three/Seven.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Four {
+public class Seven {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,7 +19,7 @@ public class Four {
 
 		for (int i = 0; i < A; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n");
+			bw.write("Case #"+(i+1)+":" + (Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+"\n");
 		}
 		br.close();
 		
