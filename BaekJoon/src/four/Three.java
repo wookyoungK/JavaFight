9f14af4682813a76d0c7a648f323690404068f1b
wookyoung <pd1125@naver.com>
2021-09-16 23:36:52 +0900

    STEP4:finish

---
+++ BaekJoon/src/four/Three.java
@@ -0,0 +1,34 @@
+package four;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//EOF
+public class Three {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		String str;
+		int N = Integer.parseInt(br.readLine());
+		
+		if(N>10) {
+			
+		}
+
+//		while ((str = br.readLine()) != null) {
+//			st = new StringTokenizer(str, " ");
+//
+//			int X = Integer.parseInt(st.nextToken());
+//			sb.append(N + X).append("\n");
+//		}
+//		
+//		System.out.println(sb);
+
+	}
+
+}
