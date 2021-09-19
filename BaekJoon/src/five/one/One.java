dc87191fbd1fed767d642576a8e23862ae8e3c83
wookyoung <pd1125@naver.com>
2021-09-19 23:49:17 +0900

    STEP5:1

---
+++ BaekJoon/src/five/one/One.java
@@ -0,0 +1,36 @@
+package five.one;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//EOF
+public class One {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int max = -1000001;
+		int min = 1000001;
+		
+		Integer.parseInt(br.readLine());
+		
+		while(st.hasMoreTokens()) {
+			int val = Integer.parseInt(st.nextToken());
+			if(val > max) {
+				max = val;
+			}
+			
+			if(val < min) {
+				min = val;
+			}
+			
+		}
+		System.out.println(min + " " + max);
+
+	}
+
+}
