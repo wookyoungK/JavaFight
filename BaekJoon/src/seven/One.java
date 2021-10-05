648f79dca1e56c584b9a65391bfdddbca6cd6611
wookyoung <pd1125@naver.com>
2021-10-05 23:58:12 +0900

    STEP7:start

---
+++ BaekJoon/src/seven/One.java
@@ -0,0 +1,17 @@
+package seven;
+
+import java.util.Scanner;
+
+public class One {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	char asc = sc.nextLine().charAt(0);
+	int num = (int)asc;
+	
+	System.out.println(num);
+		
+		
+	}
+
+}
