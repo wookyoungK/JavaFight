74ec77b88a9d072560193956d79d6296d94cf9a1
wookyoung <pd1125@naver.com>
2021-10-06 23:55:39 +0900

    STEP7:2

---
+++ BaekJoon/src/seven/Two.java
@@ -0,0 +1,24 @@
+package seven;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Two {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	int A = Integer.parseInt(bf.readLine());
+	String B = bf.readLine();
+	int sum = 0;
+	
+	for(int i=0; i<A; i++) {
+		sum += (B.charAt(i) - '0');
+			
+	}
+	System.out.println(sum);
+		
+		
+	}
+
+}
