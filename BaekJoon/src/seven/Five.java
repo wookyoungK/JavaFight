139fb04281c118dd0891b1e29de592ff40a1e936
wookyoung <pd1125@naver.com>
2021-10-11 00:08:31 +0900

    STEP7:5

--- BaekJoon/src/seven/Five.java
+++ BaekJoon/src/seven/Five.java
@@ -7,19 +7,29 @@ import java.io.InputStreamReader;
 public class Five {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-	int [] arr= new int[26];
-	String str = bf.readLine();
-	str.toUpperCase();
-	int max = -1;
-	
-	for(int i=0; i<str.length(); i++) {
-		str.charAt(i);
+		int[] arr = new int[26];
+		String str = bf.readLine();
+		str =str.toUpperCase();
+		int max = -1;
 		
-		
-	}
-	
+		for (int i = 0; i < str.length(); i++) {
+			arr[str.charAt(i) - 65]++;
+
+		}
+		char ch = '?';
+		for (int i = 0; i < 26; i++) {
+
+			if (arr[i] > max) {
+				max = arr[i];
+				ch = (char) (i + 65);
+				
+			}else if(arr[i] == max){
+				ch = '?';
+			}
+		}
+		System.out.println(ch);
 	}
 
 }
