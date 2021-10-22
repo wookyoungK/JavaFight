1f4231073e2ff6b4e3258255ee524ec8b487b319
wookyoung <pd1125@naver.com>
2021-10-22 23:52:53 +0900

    STEP8:2번

---
+++ BaekJoon/src/eight/Two.java
@@ -0,0 +1,28 @@
+package eight;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Two {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int count = 1; // 겹 수(최소 루트)
+		int range = 2;	// 범위 (최솟값 기준) 
+ 
+		if (N == 1) {
+			System.out.print(1);
+		}
+ 
+		else {
+			while (range <= N) {	// 범위가 N보다 커지기 직전까지 반복 
+				range = range + (6 * count);
+				count++;
+			}
+			System.out.print(count);
+		}
+	}
+}
