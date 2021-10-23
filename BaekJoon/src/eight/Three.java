9cec9011b5edfa296e9820dda189cd1712e99fd9
wookyoung <pd1125@naver.com>
2021-10-24 01:21:37 +0900

    STEP8:3

--- BaekJoon/src/eight/Three.java
+++ BaekJoon/src/eight/Three.java
@@ -11,19 +11,29 @@ public class Three {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int T = 2;
-		int count = 0;
-		
+		int count = 1;
+
 		if (N == 1) {
 			System.out.print("1/1");
 		}
- 
+
 		else {
-			while (range <= N) {	// 범위가 N보다 커지기 직전까지 반복 
-				range = range + (6 * count);
-				count++;
+			while (true) {
+				T++;
+				if ((count + (T - 1)) >= N) {
+					break;
+				} else {
+					count = count + (T - 1);
+				}
 			}
-			System.out.print(count);
+
+			if (T % 2 == 0) {
+				System.out.println((T - ((N - count))) + "/" + (N - count));
+			} else {
+				System.out.println((N - count) + "/" + (T - ((N - count))));
+			}
+
 		}
-		
+
 	}
 }
