c8729b5f3a3d3c546ad393c015706c07b066c472
wookyoung <pd1125@naver.com>
2021-10-25 01:02:00 +0900

    STEP8:5

---
+++ BaekJoon/src/eight/Test.java
@@ -0,0 +1,12 @@
+package eight;
+
+import java.io.IOException;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		int N = 12;
+		int A = 2;
+		System.out.println(2/10);
+	}
+}
