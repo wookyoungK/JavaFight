8e0c682f5bebfabaebed7643576c07559648ecf3
wookyoung <pd1125@naver.com>
2021-11-08 00:10:43 +0900

    STEP10: 2번

--- BaekJoon/src/ten/Two.java
+++ BaekJoon/src/ten/Two.java
@@ -1,31 +1,36 @@
 package ten;
 
+//피보나치 5 (Fibonacci numbers)
+//11.08
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//재귀함수 
-//11.07
-
-public class One {
+public class Two {
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int num = Integer.parseInt(st.nextToken());
-		System.out.println(factory(num));
+		System.out.println(fibonacci(num));
 	}
-	
-	
-	public static int factory(int num) {
+
+	public static int fibonacci(int num) {
 		int sum = 1;
-		if(num == 1 || num == 0) {
+		int temp = 1;
+		int temp2 = 1;
+		if (num == 2 || num == 1) {
 			return sum;
-		}else {
-			for(int i =1; i<=num; i++) {
-				sum = sum*i;
+		} else if (num == 0) {
+			sum = 0;
+		} else {
+			for (int i = 3; i <= num; i++) {
+				sum = temp2 + temp;
+				temp2 = temp;
+				temp = sum;
 			}
 		}
 		return sum;
