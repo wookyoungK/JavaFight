b85f6c87ffde552f4c8210472459bce4f7d1820b
wookyoungK <pd1125@naver.com>
2023-03-14 23:54:40 +0900

    D-4 수학, 문자열, 해시, 완전탐색, 조건문

--- ProgrammersBasic14/src/Five.java
+++ ProgrammersBasic14/src/Five.java
@@ -1,13 +1,20 @@
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class Five {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Five sol = new Five();
+
         String st = "hello";
-        int num = 51;
+        String s = ".... . .-.. .-.. ---";
+
+        int [] arr = {1,3,2,4,6};
+
         System.out.println("문자 반복 출력 = "+sol.st(st,3));
         System.out.println("숫자 -> 문자 출력 = "+sol.alien(51));
+        System.out.println("진료 순서 = "+sol.emergency(arr));
+        System.out.println("모스 부호 = "+sol.mos(s));
     }
 
     //문자 반복 출력
@@ -39,4 +46,31 @@ public class Five {
         return answer;
     }
 
+    public int[] emergency (int[] emergency){
+        int[] answer = new int[emergency.length];
+        int[] reverseArr = new int[emergency.length];
+        int[] arr = new int[emergency.length];
+        arr = Arrays.stream(emergency).sorted().toArray();
+        for (int i = arr.length - 1, j = 0; i >= 0; i--, j++) {
+            reverseArr[j] = arr[i];
+        }
+        for (int i = 0; i < emergency.length; i++) {
+            int target = i;
+            answer[i] =
+                    IntStream.range(0, emergency.length)
+                            .filter(num -> emergency[target] == reverseArr[num])
+                            .findFirst().getAsInt()+1;
+        }
+        return answer;
+    }
+
+    public String mos (String s){
+        String answer ="";
+        String [] str = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String[] stArr = s.split(" ");
+        for (int i = 0; i < stArr.length; i++) {
+            answer += (char) (Arrays.asList(str).indexOf(stArr[i])+97);
+        }
+        return answer;
+    }
 }
