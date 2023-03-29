0c0c482576a0fc7b34e9b0c5589f979ab224f649
wookyoungK <pd1125@naver.com>
2023-03-29 16:28:37 +0900

    컨트롤제트 -> Stack

--- ProgrammersBasic14/src/Six.java
+++ ProgrammersBasic14/src/Six.java
@@ -7,6 +7,7 @@ public class Six {
         String str = "hi12392";
 
         System.out.println("문자열 정렬 = "+sol.st(str));
+        System.out.println("컨트롤제트 = "+sol.st("10 Z 20 Z 1"));
     }
 
     //문자열 출력
@@ -27,5 +28,23 @@ public class Six {
 //        Collections.sort(answer, Collections.reverseOrder());
         return Arrays.stream(str.replaceAll("[A-Z|a-z]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray();
 
+    }
+
+    //컨트롤제트
+    public int control(String s) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (String w : s.split(" ")) {
+            if (w.equals("Z")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(w));
+            }
+        }
+        for (int i : stack) {
+            answer += i;
+        }
+        return answer;
     }
 }
