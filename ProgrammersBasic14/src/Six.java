a3ee150e366f08045c7d0d1b4a8e429062872b29
wookyoungK <pd1125@naver.com>
2023-04-03 17:38:31 +0900

    369 게임

--- ProgrammersBasic14/src/Six.java
+++ ProgrammersBasic14/src/Six.java
@@ -6,31 +6,32 @@ public class Six {
         Six sol = new Six();
         String str = "hi12392";
 
-        System.out.println("문자열 정렬 = "+sol.st(str));
-        System.out.println("컨트롤제트 = "+sol.st("10 Z 20 Z 1"));
+        System.out.println("문자열 정렬 = " + sol.st(str));
+        System.out.println("컨트롤제트 = " + sol.control("10 Z 20 Z 1"));
+        System.out.println("369 = " + sol.game(2312723));
     }
 
-    //문자열 출력
-    public int [] st (String str){
+    // 문자열 출력
+    public int[] st(String str) {
         ArrayList<Integer> answer = new ArrayList<>();
         int result = 0;
         char[] charArr = str.toCharArray();
         for (int i = 0; i < charArr.length; i++) {
-            if((int)charArr[i] <= 57){
+            if ((int) charArr[i] <= 57) {
                 answer.add(Character.getNumericValue(charArr[i]));
             }
         }
-        String strRepalce = str.replaceAll("[^0-9]","");
-        for(char ch : strRepalce.toCharArray()) {
+        String strRepalce = str.replaceAll("[^0-9]", "");
+        for (char ch : strRepalce.toCharArray()) {
             result += Character.getNumericValue(ch);
         }
 
-//        Collections.sort(answer, Collections.reverseOrder());
+        // Collections.sort(answer, Collections.reverseOrder());
         return Arrays.stream(str.replaceAll("[A-Z|a-z]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray();
 
     }
 
-    //컨트롤제트
+    // 컨트롤제트
     public int control(String s) {
         int answer = 0;
         Stack<Integer> stack = new Stack<>();
@@ -45,6 +46,23 @@ public class Six {
         for (int i : stack) {
             answer += i;
         }
+        return answer;
+    }
+
+    // 369
+    public int game(int order) {
+        int answer = 0;
+        int count = 0;
+        while (order != 0) {
+            if (order % 10 == 3 || order % 10 == 6 || order % 10 == 9) {
+                count++;
+            }
+            order = order / 10;
+        }
+        answer = count;
+        // return (int)
+        // Arrays.stream(String.valueOf(order).split("")).map(Integer::parseInt).filter(i
+        // -> i == 3 || i == 6 || i == 9).count();
         return answer;
     }
 }
