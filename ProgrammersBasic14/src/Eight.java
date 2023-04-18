6c0f61e29b93dedfff30be92b3cddc67b997c568
wookyoungK <pd1125@naver.com>
2023-04-18 23:59:33 +0900

    옹알이  -> 문자열(정규표현식) 틀림

--- ProgrammersBasic14/src/Eight.java
+++ ProgrammersBasic14/src/Eight.java
@@ -7,13 +7,13 @@ public class Eight {
         Eight sol = new Eight();
         System.out.println("안전 지대 = " + sol.safety());
         System.out.println("유한소수 판별하기 = " + sol.decimal());
-
+        System.out.println("정규표현식 문자열 찾기 = " + sol.reg());
     }
 
     // 안전 지대
     public int safety() {
-        int[][] board = { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0 },
-                { 0, 0, 0, 0, 0 } };
+        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0}};
         int answer = 0;
         boolean landmine[][] = new boolean[board.length][board.length];
 
@@ -84,5 +84,29 @@ public class Eight {
             return b;
         }
         return gcd(b, a % b);
+    }
+
+    //정규표현식 문자열 찾기
+    private static int reg() {
+        String[] babbling = {"ayaye", "uuuma", "ye", "yemawoo", "ayaa"};
+        // "aya", "ye", "woo", "ma" 오직 해당 문자열만 들어있는 것 찾기
+/*
+        해당 문제 못품
+        정규표현식의 기본은 숙지
+        캐럿(caret) 기호 ^와 달러 기호 $는 정규식에서 특별한 뜻을 지닙니다. 두 기호를 '앵커(anchor)'라고 한다.
+        캐럿 기호 ^는 텍스트의 시작, 달러 기호 $는 텍스트의 끝을 나타냄
+        ex) ^Mary 패턴은 "문자열이 시작하고 바로 Mary가 나타난다"
+            위와 유사하게 snow$를 사용해서 문자열이 snow로 끝남
+                ^...$는 문자열이 패턴과 완전히 일치하는지 확인할 때 자주 사용
+                / + / → 하나 이상의 문자
+*/
+        int answer = 0;
+        for (int i = 0; i < babbling.length; i++) {
+            if (babbling[i].matches("^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$")) {
+                answer++;
+            }
+        }
+        //result 3
+        return answer;
     }
 }
