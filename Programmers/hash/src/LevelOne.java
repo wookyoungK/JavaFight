a9357133a2a455452ab5480fbb0810cf9573ed1f
wookyoungK <pd1125@naver.com>
2023-05-05 00:49:27 +0900

    해시 Level 1 : 완주하지 못한 선수

--- Programmers/hash/src/LevelOne.java
+++ Programmers/hash/src/LevelOne.java
@@ -4,6 +4,7 @@ public class LevelOne {
     public static void main(String[] args) {
         LevelOne lv1 = new LevelOne();
         System.out.println("폰켓몬 : " + lv1.pokemon());
+        System.out.println("완주하지 못한 선수 : " + lv1.marathon());
     }
 
     private int pokemon() {
@@ -25,5 +26,30 @@ public class LevelOne {
         else
             answer = set.size();
         return answer;
+    }
+    private String marathon(){
+        String answer = "";
+        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"stanko", "ana", "mislav"};
+        Map<String, Integer> hm = new HashMap<>();
+
+        for (String part : participant) {
+            if (hm.get(part) == null) {
+                hm.put(part, 1);
+            }  else {
+                hm.put(part, hm.get(part) + 1);
+            }
+        }
+
+        for (String comp : completion) {
+            hm.put(comp, hm.get(comp) - 1);
+        }
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) == 1)
+                answer = key;
+        }
+        return answer;
+
     }
 }
