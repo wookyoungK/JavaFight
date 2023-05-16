2f415633f9d2110d8b95ed7137cc262275827377
wookyoungK <pd1125@naver.com>
2023-05-16 23:56:29 +0900

    스택/큐 : 주식가격 -> stack에서 주로 비교하는 방법

--- Programmers/src/stack/LevelTwo.java
+++ Programmers/src/stack/LevelTwo.java
@@ -7,6 +7,7 @@ public class LevelTwo {
     public static void main(String[] args) {
         LevelTwo lv2 = new LevelTwo();
         System.out.println("괄호 : " + lv2.parenthesis());
+        System.out.println("주식가격 : " + lv2.stock());
 
     }
     //괄호
@@ -24,5 +25,29 @@ public class LevelTwo {
             }  
         }  
         return stack.isEmpty();  
+    }
+
+    //주식가격
+    private int [] stock(){
+        int[] prices = { 1, 2, 3, 2, 3 };
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack();
+
+        // 순서를 stack에 저장하여 비교하는 방법 이런식으로 생각해서 많이푼다
+        // 주어진 데이터를 저장하는것이 아닌 stack을 이용하여 index 순서를 비교
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                answer[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            answer[stack.peek()] = prices.length - stack.peek() - 1;
+            stack.pop();
+        }
+
+        return answer;
     }
 }
