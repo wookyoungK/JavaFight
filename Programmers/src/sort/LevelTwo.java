985c1bef2c574533609168ffd364785bd0cbb3d4
wookyoungK <pd1125@naver.com>
2023-05-25 23:54:30 +0900

    정렬 : 소수 찾기(틀림) -> 재귀함수 및 모든경우의 수/Set

--- Programmers/src/sort/LevelTwo.java
+++ Programmers/src/sort/LevelTwo.java
@@ -6,6 +6,7 @@ public class LevelTwo {
     public static void main(String[] args) {
         LevelTwo lv2 = new LevelTwo();
         System.out.println("가장 큰 수 : " + lv2.bigNum());
+        System.out.println("소수 찾기 : "+lv2.decimal("011"));
     }
 
     // 가장 큰 수
@@ -38,4 +39,51 @@ public class LevelTwo {
         }
         return answer;
     }
+
+
+    /* 소수 찾기 */
+    HashSet<Integer> numbersSet = new HashSet<>();
+
+    //소수판별
+    private boolean isPrime(int num) {
+        if (num == 0 || num == 1)
+            return false;
+        int lim = (int)Math.sqrt(num);
+        for (int i = 2; i <= lim; i++)
+            if (num % i == 0)
+                return false;
+
+        return true;
+    }
+
+    //재귀함수
+    private void recursive(String comb, String others) {
+//        System.out.println("comb : "+comb + " others : "+ others);
+        // 1. 현재 조합을 set에 추가한다.
+        if (!comb.equals(""))
+            numbersSet.add(Integer.valueOf(comb));
+
+        // 2. 남은 숫자 중 한 개를 더해 새로운 조합을 만든다.
+        for (int i = 0; i < others.length(); i++)
+            recursive(comb + others.charAt(i), others.substring(0, i) + others.substring(i + 1));
+
+    }
+
+    private int decimal(String numbers) {
+        // 1. 모든 조합의 숫자를 만든다.
+        recursive("", numbers);
+
+        // 2. 소수의 개수만 센다.
+        int count = 0;
+        Iterator<Integer> it = numbersSet.iterator();
+        while (it.hasNext()) {
+            int number = it.next();
+            if (isPrime(number))
+                count++;
+        }
+
+        // 3. 소수의 개수를 반환한다.
+        return count;
+    }
+
 }
